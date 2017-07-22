fb9807fe5117f386baeedf8f1bab1552c51c79a1
Jaden Young <jaden.young44@gmail.com>
2017-07-22 02:52:43 -0500

    Initial project.clj setup

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject ndsu-food "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Collection of programs to improve the horrid experience of viewing NDSU restaurant and dining center menus."
+  :url "https://www.gitlab.com/jaden-young/ndsu-food"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive "1.1.6"]]
   :main ^:skip-aot ndsu-food.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:scraper {:main scraper.core
+                       :uberjar-name "ndsu-food-scraper.jar"}
+             :uberjar {:aot :all}})
