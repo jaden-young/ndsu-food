73ae1d76ff4ef4369932b4d5fa5bd6cad072afd7
Jaden Young <jaden.young44@gmail.com>
2017-07-21 19:06:42 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ndsu-food
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ndsu_food/core_test.clj
@@ -0,0 +1,7 @@
+(ns ndsu-food.core-test
+  (:require [clojure.test :refer :all]
+            [ndsu-food.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
