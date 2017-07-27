dba5e0bc995cb72d269a8e8a64f38fc40cd8c107
Jaden Young <jaden.young44@gmail.com>
2017-07-26 22:32:24 -0500

    Reorganized and simplified scraper, changed return map structure.

--- src/scraper/core.clj
+++ src/scraper/core.clj
@@ -8,36 +8,37 @@
             [clojure.string :as str])
   (:gen-class))
 
+(def ^:const loc-info {:wdc {:name "West Dining Center"
+                             :abbreviation "WDC"
+                             :num "02"}
+                       :rdc {:name "Residence Dining Center"
+                             :abbreviation "RDC"
+                             :num "04"}
+                       :udc {:name "Union Dining Center"
+                             :abbreviation "UDC"
+                             :num "10"}
+                       :pe {:name "Pizza Express"
+                            :abbreviation "PE"
+                            :num "25"}
+                       :mg {:name "Marketplace Grille"
+                            :abbreviation "MG"
+                            :num "11"}})
+
 ;; *****************************************************************************
 ;; Grab pages
 ;; *****************************************************************************
 (def ^:const base-url "https://www.ndsu.edu/dining/menu/shortmenu.asp")
 (def website-date-formatter (t-fmt/formatter "M/d/yyyy"))
 
-;; Key to identify restaurant with website
-(def ^:const loc-nums {:wdc "02"
-                       :rdc "04"
-                       :udc "10"
-                       :pe "25"
-                       :mg "11"})
-(def ^:const all-locs (vec (keys loc-nums)))
-
 (def cache-dir
   ;; One would assume that a directory name ends in '/', but I guess Java
-  ;; has it's reasons.
+  ;; has its reasons.
   (let [home (System/getProperty "user.home")
         folders [home ".cache" "ndsu-food" "html"]
         path (interpose (java.io.File/separatorChar) folders)
         s (apply str path)
         dir (str s (java.io.File/separatorChar))]
     dir))
-
-(defn- loc-abbrev
-  [loc]
-  (-> loc
-      (str)
-      (subs 1)
-      (string/upper-case)))
 
 (defn iso-date-fmt
   [date]
@@ -53,13 +54,13 @@
                     (str)
                     (subs 1)
                     (str/upper-case))]
-  (str (iso-date-fmt date) "-" loc-str ".html")))
+    (str (iso-date-fmt date) "-" loc-str ".html")))
 
 (defn- build-url
   [date loc]
   (let [u (url/url base-url)
         q {:dtdate (menu-date-fmt date)
-           :locationNum (get loc-nums loc)}]
+           :locationNum (get-in loc-info [loc :num])}]
     (str (assoc u :query q))))
 
 (defn- persist!
@@ -71,31 +72,26 @@
       (io/copy (build-url date loc) file))
     file))
 
-(defn- locs-resources
-  [date locs resource-fn]
-  (into {} (map #(vector % (resource-fn date %)) locs)))
+(defn- grab
+  [no-cache]
+  (if no-cache
+    build-url
+    persist!))
 
-(defn grab
-  "Creates a mapping from location keywords to urls of
-  menus for those dates/locs on the web. If locations are not
-  provided, defaults to all."
-  ([date]
-   (apply grab date all-locs))
-  ([date & locs]
-   (locs-resources date locs build-url)))
-
-(defn grab!
-  "Creates a mapping from location keywords to java.io.File objects with html
-  data for the menus at that loc/date. Grabs the menu data from the web and
-  stores it at ~/.cache/ndsu-food/html/. Cache is checked each time, so web
-  is only hit once."
-  ([date]
-   (apply grab! date all-locs))
-  ([date & locs]
-   (locs-resources date locs persist!)))
 ;; *****************************************************************************
 ;; Scrape pages
 ;; *****************************************************************************
+
+(defn- title-case-word
+  [w]
+  (if (zero? (count w))
+    w
+    (str (Character/toTitleCase (first w))
+         (subs w 1))))
+
+
+;;; Selectors. These functions take html data processed by enlive.
+
 (defn- gluten-free?
   [row]
   (not (empty? (select row [(attr-contains :src "gluten")]))))
@@ -104,33 +100,46 @@
   [row]
   (not (empty? (select row [(attr-contains :src "veggie")]))))
 
-(defn- food-item
-  "Selects a food item from a row"
+(defn- nuts?
   [row]
-  ;; The last character is some kind of space, but
-  ;; clojure doesn't recognize it as such, so trim
-  ;; doesn't work. There has to be a more efficient way of doing this.
-  ;; HACK: Fix unnecessary conversions
-  (let [name (->> (select row [:div.shortmenurecipes :span])
-                  (map html/text)
-                  (apply str)
-                  (butlast)
-                  (apply str))]
-    {:name (string/replace name #" - Vegetarian" "")
-     :gluten-free (gluten-free? row)
-     :vegetarian (vegetarian? row)}))
+  (not (empty? (select row [(attr-contains :src "nuts")]))))
 
 (defn- category?
   "Tells whether a given row contains a category (true) or a food item (false)"
   [row]
   (not (empty? (select row [:div.shortmenucats]))))
 
-(defn- ->kebob-case
-  "Not rigorous but handles the case here"
-  [s]
-  (-> s
-      (string/lower-case)
-      (string/replace #" " "-")))
+(defn- food-item
+  "Selects a food item from a row"
+  [row]
+  (let [name (->> (select row [:div.shortmenurecipes :span])
+                  (first)
+                  (html/text) ; the last char of the name is some sort of space,
+                  (butlast)   ; but string/trim doesn't get rid of it.
+                  (apply str))]
+    {:name (string/replace name #" - Vegetarian" "")
+     :gluten-free (gluten-free? row)
+     :vegetarian (vegetarian? row)
+     :nuts (nuts? row)}))
+
+(defn- meals
+  "Selects a map of nodes where each top-level entry is a meal"
+  [page]
+  (select page
+          [[:table (attr= :cellspacing "0") (attr= :cellpadding "0")
+            (attr= :border "0") (attr= :width "100%") (attr= :height "100%")]]))
+
+(defn- meal-name
+  "Selects the meal name of the first meal found as a string"
+  [page]
+  (->> (select page [:div.shortmenumeals])
+       (map html/text)
+       (first)))
+
+(defn- meal-data
+  "Selects the categories and menu items from a meal page"
+  [page]
+  (select page [:table (attr= :cellspacing "1") :> :tr]))
 
 (defn- category-name
   [row]
@@ -141,85 +150,64 @@
                              (drop 3)
                              (drop-last 3)
                              (apply str)
-                             (->kebob-case))))]
+                             (str/lower-case)
+                             (title-case-word))))]
     ;; I don't want to deal with escaping that slash everywhere in the
     ;; application. "soups" carries just as much meaning as "soups/chowders"
-    (string/replace (first name) #"/chowders" "")))
+    (str/replace (first name) #"/chowders" "")))
 
 (defn- categorize
-  "Creates a map of {:category [food-item]}"
   [meal-info]
   (let [cats-and-items (partition 2 (partition-by category? meal-info))]
-    (into {} (for [[[cat] foods] cats-and-items]
-               (let [k (keyword (category-name cat))
-                     v (vec (map food-item foods))]
-                 [k v])))))
+    (vec (for [[[cat] foods] cats-and-items]
+           (let [name (category-name cat)
+                 items (vec (map food-item foods))]
+             {:name name
+              :items items})))))
 
-(defn- meals
-  "Selects a map of nodes where each top-level entry is a meal"
+(defn- ->meal
   [page]
-  (select page
-          [[:table (attr= :cellspacing "0") (attr= :cellpadding "0")
-            (attr= :border "0") (attr= :width "100%") (attr= :height "100%")]]))
+  (let [name (meal-name page)
+        data (categorize (meal-data page))]
+    {:name name
+     :categories data}))
 
-(defn- meal-name
-  "Selects the meal name as a string"
-  [meal]
-  (->> (select meal [:div.shortmenumeals])
-       (map html/text)
-       (first)
-       (string/lower-case)))
-
-(defn- meal-data
-  "Selects the categories and menu items from a meal"
-  [meal]
-  (select meal [:table (attr= :cellspacing "1") :> :tr]))
-
-(defn- meal
-  [m]
-  (let [name (meal-name m)
-        data (meal-data m)]
-    {(keyword name) (categorize data)}))
-
-(defn- fetch-page
-  [file]
-  (with-open [rdr (io/reader file)]
+(defn- read-page
+  [src]
+  (with-open [rdr (io/reader src)]
     (html/html-resource rdr)))
 
-(defn html->menu
-  "Extracts a menu map from an html document. Argument must be able to
-  be coerced to a java.io.Reader"
+(defn- ->meals
+  "Extracts a sequence of meals maps from an html document. Argument must be
+  able to be coerced to a java.io.Reader."
   [html]
-  (let [page (fetch-page html)
-        ms (meals page)]
-    (into {} (map meal ms))))
+  (let [page (read-page html)
+        meals (map ->meal (meals page))]
+    meals))
 
-(defn- fmap
-  [f m]
-  (into (empty m) (for [[k v] m] [k (f v)])))
-
-(defn- do-scrape
-  [date locs grabber]
-  (let [srcs (apply grabber date locs)
-        menus (fmap html->menu srcs)]
-    {:date (iso-date-fmt date)
-     :menus menus}))
-
-(defn scrape
-  "Scrapes menu data from the web for the given date/locs, returning a map.
-  If no locations are provided, all are scraped. Results are not cached."
-  ([date]
-   (apply scrape date all-locs))
-  ([date & locs]
-   (do-scrape date locs grab)))
+(defn- scrape-one
+  ([grabber date loc]
+   (let [src (grabber date loc)]
+     {:date (iso-date-fmt date)
+      :restaurant (get loc-info loc)
+      :meals (->meals src)})))
 
 (defn scrape!
-  "Same as scrape, but persists html files to ~/.cache/ndsu-food/html/.
-  Cache is checked on each run, so web is only hit once."
-  ([date]
-   (apply scrape! date all-locs))
-  ([date & locs]
-   (do-scrape date locs grab!)))
+  "Scrapes the NDSU site for menu data on the given date, returning a sequence
+  of menus. To specify which restaurants are scraped, pass any number of: :wdc
+  :rdc :udc :pe :mg, Otherwise all menus are scraped. Scraped html pages are
+  persisted to ~/.cache/ndsu-food/html/ unless :no-cache is passed as an arg.
+  :no-cache always forces a new hit to the web, otherwise cache is checked
+  first. Archiving the pages may be advisable since past pages are not available
+  from the NDSU site."
+  [date & flags]
+  (let [flags (set flags)
+        grabber (grab (contains? flags :no-cache))
+        fs (disj flags :no-cache)
+        locs (if (empty? fs)
+               (keys loc-info)
+               fs)]
+    (map (partial scrape-one grabber date) locs)))
 
 (defn -main
   [& args]
