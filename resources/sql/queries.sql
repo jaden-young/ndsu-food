54d3719160ba41d37d254a119c5b153ab59f1d4f
Jaden Young <jaden.young44@gmail.com>
2017-07-29 22:17:12 -0500

    Added a few basic query functions and a minimal test

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,69 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- :name create-food-item! :i
+-- :doc creates a new food item, returning the auto-generated id
+INSERT INTO food_item
+(name, vegetarian, gluten_free, nuts)
+VALUES (:name, :vegetarian, :gluten_free, :nuts)
+RETURNING id
 
--- :name update-user! :! :n
--- :doc update an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
+-- :name all-food-items :? :*
+-- :doc gets every food item
+SELECT id, name, vegetarian, gluten_free, nuts
+FROM food_item
+
+-- :name food-item :? :1
+-- :doc get a single food-item by id
+SELECT id, name, vegetarian, gluten_free, nuts
+FROM food_item
 WHERE id = :id
 
--- :name get-user :? :1
--- :doc retrieve a user given the id.
-SELECT * FROM users
+-- :name food-item-by-name :? :1
+-- :doc get a single food item with a matching name
+SELECT id, name, vegetarian, gluten_free, nuts
+FROM food_item
+WHERE name = :name
+
+-- :name all-restaurants :? :*
+-- :doc gets every restaurant
+SELECT id, name, abbreviation
+FROM restaurant
+
+-- :name restaurant :? :1
+-- :doc gets a single restaurant by id
+SELECT id, name, abbreviation
+FROM restaurant
 WHERE id = :id
 
--- :name delete-user! :! :n
--- :doc delete a user given the id
-DELETE FROM users
-WHERE id = :id
+-- :name restaurant-by-name :? :1
+-- :doc gets a single restaurant with matching name
+SELECT id, name, abbreviation
+FROM restaurant
+WHERE name = :name
+
+-- :name restaurant-by-abbreviation :? :1
+-- :doc gets a single restaurant with matching abbreviation
+SELECT id, name, abbreviation
+FROM restaurant
+WHERE abbreviation = :abbreviation
+
+-- :name served-on-date :? :*
+-- :doc gets all items served on a given date
+SELECT
+    fi.name AS food_item_name,
+    fi.vegetarian, fi.gluten_free, fi.nuts,
+    r.name AS restaurant_name,
+    sa.date, sa.meal AS meal, sa.category AS category
+FROM
+    food_item AS fi
+      JOIN served_at AS sa
+          ON fi.id = sa.food_item_id
+      JOIN restaurant AS r
+          ON r.id = sa.restaurant_id
+WHERE
+    sa.date = :date
+ORDER BY
+    meal, r.name, category
+
+-- :name insert-menu-data! :! :n
+-- :doc inserts menu data
+INSERT INTO served_at (date, meal, category, food_item_id, restaurant_id)
+VALUES (:date, :meal, :category, :food_item_id, :restaurant_id)
