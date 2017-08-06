b81da65fecccf65dceb1d08dcd3f151ef3588eb7
Jaden Young <jaden.young44@gmail.com>
2017-08-06 17:07:14 -0500

    Added api endpoint to query menu by date
    I know that 'GET /menu' should list resources, but as this is a simple hobby
    project and I don't want to deal with pagination right now, I'm defaulting
    to only the current day's menu.

--- src/clj/ndsu_food/routes/services.clj
+++ src/clj/ndsu_food/routes/services.clj
@@ -1,44 +1,62 @@
 (ns ndsu-food.routes.services
   (:require [ring.util.http-response :refer :all]
             [compojure.api.sweet :refer :all]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [clj-time.core :as t]
+            [clj-time.format :as t-fmt]
+            [ndsu-food.util :as util]
+            [ndsu-food.db.core :as db]
+            [cheshire.core :as c]))
+
+(s/defschema Food-Item
+  {:name s/Str
+   :vegetarian s/Bool
+   :gluten-free s/Bool
+   :nuts s/Bool})
+
+(s/defschema Category
+  {:name s/Str
+   :items [Food-Item]})
+
+(s/defschema Restaurant
+  {:name s/Str
+   :categories [Category]})
+
+(s/defschema Meal
+  {:name s/Str
+   :restaurants [Restaurant]})
+
+(s/defschema Menu
+  {:date s/Str
+   :meals [Meal]})
+
+(defn date-string?
+  [date]
+  (some? (t-fmt/parse (t-fmt/formatters :date) date)))
+
+(def Date-String (describe
+                  (s/constrained s/Str date-string?)
+                  "ISO-8601 \"YYYY-MM-DD\" formatted date string"))
 
 (defapi service-routes
   {:swagger {:ui "/swagger-ui"
              :spec "/swagger.json"
              :data {:info {:version "1.0.0"
-                           :title "Sample API"
-                           :description "Sample Services"}}}}
-  
+                           :title "NDSU Food"
+                           :description "Access NDSU Dining menu information"}}}}
+
   (context "/api" []
-    :tags ["thingie"]
+    :tags ["Menu"]
 
-    (GET "/plus" []
-      :return       Long
-      :query-params [x :- Long, {y :- Long 1}]
-      :summary      "x+y with query-parameters. y defaults to 1."
-      (ok (+ x y)))
-
-    (POST "/minus" []
-      :return      Long
-      :body-params [x :- Long, y :- Long]
-      :summary     "x-y with body-parameters."
-      (ok (- x y)))
-
-    (GET "/times/:x/:y" []
-      :return      Long
-      :path-params [x :- Long, y :- Long]
-      :summary     "x*y with path-parameters"
-      (ok (* x y)))
-
-    (POST "/divide" []
-      :return      Double
-      :form-params [x :- Long, y :- Long]
-      :summary     "x/y with form-parameters"
-      (ok (/ x y)))
-
-    (GET "/power" []
-      :return      Long
-      :header-params [x :- Long, y :- Long]
-      :summary     "x^y with header-parameters"
-      (ok (long (Math/pow x y))))))
+    (GET "/menu" []
+      :return Menu
+      :query-params [{date :- Date-String (util/today-str)}]
+      :summary "menu for date, defaults to today"
+      (let [d (util/parse-date-add-default-zone date)
+            menu (-> {:date d}
+                     (db/get-menus-on-date)
+                     (db/hierarchize-menu-on-date)
+                     (first))]
+        (if menu
+          (ok menu)
+          (not-found {:errors (str "No menu data for " date)}))))))
--- src/clj/ndsu_food/util.clj
+++ src/clj/ndsu_food/util.clj
@@ -1,6 +1,26 @@
 (ns ndsu-food.util
-  (:require [clj-time.format :as t-fmt]))
+  (:require [clj-time.format :as t-fmt]
+            [clj-time.core :as t]))
 
 (defn iso-date-fmt
   [date]
   (t-fmt/unparse (t-fmt/formatters :date) date))
+
+(defn today-str
+  []
+  (t-fmt/unparse-local-date (t-fmt/formatters :date) (t/today)))
+
+(defn parse-date-add-default-zone
+  "Parses a 'yyyy-mm-dd' string into a clj-time.DateTime object, adding
+  the time zone, or default to server's time zone.
+
+  When given a string 'yyyy-mm-dd' clj-time parses it into a DateTime with
+  zeroed out UTC time. This causes the parsed date to be one day behind/ahead
+  depending on the timezone. It would be a better idea to figure out how to
+  standardize on UTC instead of converting these time zones, but that's a job
+  for another day."
+  ([date-str]
+   (parse-date-add-default-zone date-str (t/default-time-zone)))
+  ([date-str zone]
+   (let [fmt (t-fmt/with-zone (t-fmt/formatter :date) zone)]
+     (t-fmt/parse fmt date-str))))
