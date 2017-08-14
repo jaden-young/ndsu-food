f2cbd5e142631f2d61f7641577f9daff32d3a627
jaden <jaden.young44@gmail.com>
2017-08-14 14:52:26 -0500

    more simple user.clj imports

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -1,10 +1,12 @@
 (ns user
   (:require [clj-time.core :as t]
+            [clj-time.format :as t-fmt]
             [migratus.core :as mig]
             [mount.core :as mount]
             [ndsu-food.config :refer [env]]
             ndsu-food.core
-            [ndsu-food.db.core :as db]))
+            [ndsu-food.db.core :as db]
+            [ndsu-food.util :as util]))
 
 (def db-map {:store :database
                  :db (env :database-url)})
